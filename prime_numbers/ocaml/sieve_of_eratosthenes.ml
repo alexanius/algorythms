95537602a5bde4d0dca5d3ae4047f7b96f3b1a64
alexanius <alexanius@gmail.com>
2013-01-16 23:13:43 +0000

    Adding documentation to a module

--- prime_numbers/ocaml/sieve_of_eratosthenes.ml
+++ prime_numbers/ocaml/sieve_of_eratosthenes.ml
@@ -1,36 +1,56 @@
+(**
+evaluation of prime numbers with sieve of eratosthenes algorytm
 
-(* Function sowing the array a of boolean from 2nd element to the end of array
- * and sets element with not a prime index to `false'. In the end of an algorythm
- * only elements with prime indices will be `true'. *)
+@version 0.01
+@author alexanius
+ *)
+
+(**
+@param a array of boolean where true means prime
+@param max left bound of counting prime numbers
+
+Function sowing the array a of boolean from 2nd element to the end of array
+and sets element with not a prime index to `false'. In the end of an algorythm
+only elements with prime indices will be `true'.
+ *)
 let sowing = fun a max ->
-	(* Walks throu the array `a' and sets dividents of `i' to false *)
+(**
+@param a array of boolean where true means prime
+@param i current index
+@param step index incrementing step
+@param max left bound of counting prime numbers
+
+Walks through the array `a' and sets dividents of `i' to false
+ *)
 	let rec sowing_tail = fun a i step max ->
 		if i < max then
 			(Array.set a i false;
 			sowing_tail a (i + step) step max)
 	in
-	(* search first `true' element starting from `i' *)
+(**
+@param a array of boolean where true means prime
+@param i current index
+@param max left bound of counting prime numbers
+
+search first `true' element starting from `i'
+ *)
 	let rec sowing_head = fun a i max ->
 		if i * i < max then
 			(if (Array.get a i) = true then
-				sowing_tail a (i + i) i max;								(* if number is prime then delete all it's dividents *)
-			sowing_head a (i + 1) max		(* if not - just continue *)
+				sowing_tail a (i + i) i max;	(*** if number is prime then delete all it's dividents *)
+			sowing_head a (i + 1) max			(*** if not - just continue *)
 			)
 	in
-	(* start counting and return prime number quantity *)
+	(*** start counting and return prime number quantity *)
 	sowing_head a 2 max
 ;;
 
-(*let print_prime_numbers = fun a ->
-	let rec print_prime_number = fun a i max ->
-		if (Array.get a i) = true then
-		Printf.printf "%d %!" i;
-		if i < max then print_prime_number a (i+1) max
-		else Printf.printf "\n%!";
-	in
-	print_prime_number a 0 ((Array.length a) - 1)
-;;*)
+(*
+@param a array of bool where a[i] == true menas that a is prime
+@return list of prime numbers
 
+Convert array of numbers to list, putting there only prime numbers
+*)
 let prime_num_array_to_list = fun a ->
 	let rec prime_num_array_to_list_internal = fun a i l ->
 		if Array.length a == i then
@@ -44,14 +64,16 @@ let prime_num_array_to_list = fun a ->
 	prime_num_array_to_list_internal a 2 []
 ;;
 
-(* Function takes a number, wich is the left border for searching prime numbers
- * and returns a list of prime numbers
- * *)
+(**
+@param x left bound of counting prime numbers
+
+Count all prime numbers that are less than x
+*)
 let sieve_of_eratosthenes = fun x ->
-	let a = Array.make (read_int() + 1) true		(* `+ 1' because it's easy when element index is equal to real number. So we have a[0] -> 0, a[1] -> 1 *)
+	let a = Array.make (read_int() + 1) true		(** `+ 1' because it's easy when element index is equal to real number. So we have a[0] -> 0, a[1] -> 1 *)
 	in
-	Array.set a 0 false;		(* we know, it's not prime *)
-	Array.set a 1 false;		(* we know, it's not prime *)
+	Array.set a 0 false;		(** we know, it's not prime *)
+	Array.set a 1 false;		(** we know, it's not prime *)
 	sowing a (Array.length a);
 	prime_num_array_to_list a;
 ;;
